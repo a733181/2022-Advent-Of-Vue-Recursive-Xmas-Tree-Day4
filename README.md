1054a28b6b4fae5490629eb767b64c0db2ddc2ad
Syuan <a733181@gmail.com>
2022-12-29 08:11:05 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# 2022-Advent-Of-Vue-Recursive-Xmas-Tree-Day4
+# Advent Of Vue 2022
+
+## 2022-Advent-Of-Vue-Recursive-Xmas-Tree-Day4
+
+[Challenge URL](https://github.com/Advent-Of-Vue/2022-recursive-tree-problem?utm_campaign=Advent%20Of%20Vue&utm_medium=email&utm_source=Revue%20newsletter)
---
+++ src/ChristmasTree.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="flex flex-row justify-center">
+      <!-- This div is styled for the tree sections, but it's not complete -->
+      <div class="relative rounded-full bg-green w-16 h-16 -m-2 flex justify-center items-center" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  size: {
+    type: Number,
+    default: 1,
+  },
+})
+</script>
