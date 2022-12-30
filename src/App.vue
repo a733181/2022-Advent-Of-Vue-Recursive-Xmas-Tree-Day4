5116d957ccbdeef0b8e5bd599a52afe0cc698330
Syuan <a733181@gmail.com>
2022-12-30 08:30:46 +0800

    新增聖誕樹

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,32 @@
 <template>
-  <div class="w-full h-full flex justify-center items-center">
-    <ChristmasTree :size="7" />
+  <div class="w-full h-full flex justify-center items-center flex-col">
+    <ChristmasTree :size="size" />
+    <input
+      type="text"
+      v-model="inputSize"
+      class="mt-10 p-2 text-green placeholder:text-yellow-800 border border-green focus:border-blue-300 outline-none"
+      :class="{ 'border-red-200': inputSize > 10 }"
+      @change="chirstmasTreeSizeHandler"
+      placeholder="請輸入聖誕樹高度"
+    />
+    <p v-if="inputSize > 10" class="text-red-300">聖誕樹高度最多為10</p>
+    <p v-if="isNaN(Number(inputSize)) && inputSize !== ''">請輸入數字</p>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import ChristmasTree from './ChristmasTree.vue'
+
+const inputSize = ref('')
+const size = ref()
+const chirstmasTreeSizeHandler = () => {
+  if (inputSize.value > 10) {
+    size.value = 10
+  } else if (!isNaN(Number(inputSize.value))) {
+    size.value = Number(inputSize.value)
+  } else {
+    size.value = 0
+  }
+}
 </script>
--- src/ChristmasTree.vue
+++ src/ChristmasTree.vue
@@ -1,8 +1,13 @@
 <template>
   <div>
-    <div class="flex flex-row justify-center">
+    <div v-for="(num, index) in size" :key="index" class="flex flex-row justify-center">
       <!-- This div is styled for the tree sections, but it's not complete -->
-      <div class="relative rounded-full bg-green w-16 h-16 -m-2 flex justify-center items-center" />
+      <div
+        v-for="(item, Iindex) in num"
+        :key="Iindex"
+        class="relative rounded-full w-16 h-16 -m-2 flex justify-center items-center"
+        :class="{ 'bg-green': num % 2 === 0, 'bg-red-300': num % 2 !== 0 }"
+      />
     </div>
   </div>
 </template>
@@ -11,7 +16,7 @@
 const props = defineProps({
   size: {
     type: Number,
-    default: 1,
+    default: 0,
   },
 })
 </script>
